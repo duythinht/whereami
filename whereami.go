67289eaa7905e33befa5660e3ca77e7a66c9f1f3
Thinh Tran <duythinht@gmail.com>
2016-04-18 16:04:37 +0700

    Add version and statsd support

--- whereami.go
+++ whereami.go
@@ -2,24 +2,31 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 
+	"github.com/cactus/go-statsd-client/statsd"
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
 )
 
-var count int32
-
 func main() {
 	fmt.Println("Bootstrap Where Am I server...")
+
+	var count int64
+	client, _ := statsd.NewClient("108.60.126.255:8125", "test-client")
 
 	m := martini.Classic()
 	m.Use(render.Renderer())
 
+	m.Use(func(req http.Request) {
+		client.Timing("response.GET./.200", count, 1.0)
+	})
+
 	m.Get("/", func(r render.Render) {
 		hostname, _ := os.Hostname()
 		count++
-		r.JSON(200, map[string]interface{}{"ok": true, "hostname": hostname, "count": count})
+		r.JSON(200, map[string]interface{}{"ok": true, "data": map[string]interface{}{"hostname": hostname, "count": count, "version": 1}})
 	})
 
 	m.Run()
