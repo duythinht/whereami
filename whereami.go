498a2ed3d398cbc77bcbf80fe1f26b31cfa6642f
Thinh Tran <duythinht@gmail.com>
2016-04-19 10:05:10 +0700

    To the version 3

--- whereami.go
+++ whereami.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/cactus/go-statsd-client/statsd"
 	"github.com/go-martini/martini"
@@ -15,29 +16,40 @@ func main() {
 	fmt.Println("Bootstrap Where Am I server...")
 
 	var count int64
-	client, err := statsd.NewClient("108.61.126.255:8125", "whereami")
+
+	client, err := statsd.NewClient("108.61.126.225:8125", "")
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	defer client.Close()
-
-	client.Inc("stat1", 42, 1.0)
-
 	m := martini.Classic()
 	m.Use(render.Renderer())
 
-	m.Use(func(c martini.Context, req *http.Request, log *log.Logger) {
+	m.Use(func(c martini.Context, req *http.Request, log *log.Logger, res http.ResponseWriter) {
+		begin := time.Now().Nanosecond() / 1000
+
 		c.Next()
-		client.Timing("response.GET./.200", count, 1.0)
-		log.Println("After request	")
+
+		end := time.Now().Nanosecond() / 1000
+		// Wrap http.ResponseWrite to martiti.ResponseWrite
+		rw := res.(martini.ResponseWriter)
+
+		telemetryKey := fmt.Sprintf("whereami.response.%s.%s.%v", req.Method, req.URL.Path, rw.Status())
+		client.Timing(telemetryKey, int64(end-begin), 1.0)
 	})
 
 	m.Get("/", func(r render.Render) {
 		hostname, _ := os.Hostname()
 		count++
-		r.JSON(200, map[string]interface{}{"ok": true, "data": map[string]interface{}{"hostname": hostname, "count": count, "version": 1}})
+		r.JSON(200, map[string]interface{}{
+			"ok": true,
+			"data": map[string]interface{}{
+				"hostname": hostname,
+				"count":    count,
+				"version":  3,
+			},
+		})
 	})
 
 	m.Run()
