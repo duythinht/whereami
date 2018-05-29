ca69231c2d9cd4f06bd21499e3687a68817663e9
Thinh Tran <duythinht@gmail.com>
2018-05-29 14:56:03 +0900

    Remove unused code

--- whereami.go
+++ whereami.go
@@ -3,11 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
 	"os"
-	"time"
 
-	"github.com/cactus/go-statsd-client/statsd"
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
 )
@@ -17,27 +14,12 @@ func main() {
 
 	var count int64
 
-	client, err := statsd.NewClient("108.61.126.225:8125", "")
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	m := martini.Classic()
 	m.Use(render.Renderer())
-
-	m.Use(func(c martini.Context, req *http.Request, log *log.Logger, res http.ResponseWriter) {
-		begin := time.Now().Nanosecond() / 1000
-
-		c.Next()
-
-		end := time.Now().Nanosecond() / 1000
-		// Wrap http.ResponseWrite to martiti.ResponseWrite
-		rw := res.(martini.ResponseWriter)
-
-		telemetryKey := fmt.Sprintf("whereami.response.%s.%s.%v", req.Method, req.URL.Path, rw.Status())
-		client.Timing(telemetryKey, int64(end-begin), 1.0)
-	})
 
 	m.Get("/", func(r render.Render) {
 		hostname, _ := os.Hostname()
